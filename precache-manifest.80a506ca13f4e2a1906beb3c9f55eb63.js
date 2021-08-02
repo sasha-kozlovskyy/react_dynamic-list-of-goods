ad9d0e0f1a695935bdb5ef5f582e1798c9794309
Sasha Kozlovskyy <alex33507@gmail.com>
2021-08-02 16:45:12 +0300

    Updates

---
+++ precache-manifest.80a506ca13f4e2a1906beb3c9f55eb63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f98a6d8e202bbd4998b6ea8f406126",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b323ac5df3b6966b1f47",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "965d47e073d551d08910",
+    "url": "./static/js/2.98e7adc1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.98e7adc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b323ac5df3b6966b1f47",
+    "url": "./static/js/main.484bd5af.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
